5940efdca6d4409a57c2ecdb8256d7511fcbf781
Gabriel Mero <og.gabrielmero@gmail.com>
2022-02-14 13:50:04 -0300

    Submit

--- hackaton.py
+++ hackaton.py
@@ -1,1 +1,9 @@
-# Seu código aqui
+hackathon_1 = ["Team Kenzie", "Team Ateliware", "Team VHSYS", "Team Mirum"]
+hackathon_2 = ["Team Ateliware", "Team Kenzie", "Team VHSYS", "Team Mirum"]
+hackathon_3 = ["Team Mirum", "Team Ateliware","Team VHSYS", "Team Kenzie"]
+
+def get_score(team_name, teams):
+    pos = teams.index(team_name) + 1
+    res = f'A {team_name} ficou classificada em {pos}'
+
+    return res
